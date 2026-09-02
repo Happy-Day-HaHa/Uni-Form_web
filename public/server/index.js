7f7a2f64f83a20c333eed24c78084b16526a4c6d
Happy-Day-HaHa <kimyo3538@gmail.com>
2026-09-02 13:27:49 +0900

    Fix SPA routing for Sites deployment

--- public/server/index.js
+++ public/server/index.js
@@ -1,5 +1,13 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url)
+    const acceptsHtml = request.headers.get('accept')?.includes('text/html')
+
+    if (acceptsHtml && !url.pathname.split('/').pop()?.includes('.')) {
+      url.pathname = '/index.html'
+      return env.ASSETS.fetch(new Request(url, request))
+    }
+
     return env.ASSETS.fetch(request)
   },
 }
